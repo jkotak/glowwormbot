c15c954b098ce033281f962e110b202f5a518e12
jkotak <kotakj@gmail.com>
2017-07-16 14:32:13 -0700

    Update postbacks.js

--- modules/postbacks.js
+++ modules/postbacks.js
@@ -1,20 +1,12 @@
 "use strict";
 
-let salesforce = require('./salesforce'),
-    messenger = require('./messenger'),
+let messenger = require('./messenger'),
     formatter = require('./formatter'),
     loanapplicationhandler = require('./loanapplicationhandler'),
     userinfohandler = require('./userinfohandler'),
     casehandler = require('./casehandler');
 
 
-exports.schedule_visit = (sender, values) => {
-    console.log("Values " + values);
-    console.log("Values1 " + values[1]);
-    salesforce.findProperties({id: values[1]}).then(properties => {
-        messenger.send(formatter.formatAppointment(properties[0]), sender);
-    });
-};
 
 exports.contact_broker = (sender, values) => {
     messenger.send({text: "Here is the realtor information for this property"}, sender);
@@ -25,22 +17,7 @@ exports.confirm_visit = (sender, values) => {
     messenger.send({text: `OK, your appointment is confirmed for ${values[2]}. ${values[1]}.`}, sender);
 };
 
-exports.show_rates = (sender,values) => {
-    console.log("Values " + values);
-    console.log("Values1 " + values[1]);
-    salesforce.findRate({productType: values[1]}).then(products => {
-        console.log("Values " + products);
-        messenger.send(formatter.formatProducts(products), sender);
-    });
-};
 
-exports.contact_me = (sender, values) => {
-    let propertyId = values[1];
-    messenger.send({text: `Thanks for your interest. I asked the loan officer to contact you asap. Before you leave, could you please provide feedback to my creator at http://bit.ly/2qOPctw ?`}, sender);
-    messenger.getUserInfo(sender).then(response => {
-        salesforce.createLead(propertyId, response.first_name, response.last_name, sender);
-    });
-};
 
 exports.houses_near_me = (sender, values) => {
     messenger.send(formatter.requestLocation(), sender);
@@ -67,18 +44,7 @@ exports.contact_support = (sender, values) => {
 };
 
 
-exports.loan_status = (sender,values) =>{
-    userinfohandler.findUserHistoryWithProjections(sender,{salesforce_id: 1}).then(user => { 
-        console.log('User' + user.id + user.salesforce_id);
-        if(user.salesforce_id!=null){
-            salesforce.getLoanStatus(user.salesforce_id).then(loans => {
-                messenger.send(formatter.formatLoans(loans), sender);
-            });
-        }else{
-            messenger.send(formatter.formatLoanAccountLinking(), sender);
-        }
-    });
-};
+
 
 
 exports.occupancy_type = (sender,values) => {
