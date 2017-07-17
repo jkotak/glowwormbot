7397df3326eaa62e98f3d0afa129d4d5c6dfb653
jkotak <kotakj@gmail.com>
2017-07-16 20:33:22 -0700

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -5,7 +5,7 @@ let messenger = require('./messenger'),
 
 exports.thankYou = (sender) => {
     messenger.getUserInfo(sender).then(response => {
-        messenger.send({text: `You're welcome, ${response.first_name}! I am happy to help. You could also provide feedback to my creator at http://bit.ly/2qOPctw`}, sender);
+        messenger.send({text: `You're welcome, ${response.first_name}! I am happy to help.`}, sender);
     });
 };
 
@@ -18,6 +18,19 @@ exports.help = (sender) => {
         messenger.send(formatter.formatQuickReplies(text,postback,options), sender);
     });
 };
+
+exports.searchByCategory = (sender) => {
+    messenger.getUserInfo(sender).then(response => {
+        let text = 'Sure thing, '+response.first_name+'. I can help you search our rates in two ways: You can either search by category or by uploading a picture; pick an option below.'
+        let postback = ['searchByCategory','searchByImage']
+        let options = ['By Categories','By Image'];
+        messenger.send(formatter.formatQuickReplies(text,postback,options), sender);
+    });
+};
+exports.searchByImage = (sender) => {
+    messenger.send({text: `OK, you can upload the image using messenger. Either take a picture of the product or just upload an image from your library. Go ahead and upload the picture.. I will wait`}, sender);
+};
+
 
 
 
