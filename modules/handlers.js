58de35dc53ea17625e0a70cc9ce171368d7371f1
jkotak <kotakj@gmail.com>
2017-07-17 00:55:28 -0700

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let messenger = require('./messenger'),
+    producthandler = require('./producthandler'),
     formatter = require('./formatter');
 
 exports.thankYou = (sender) => {
@@ -21,10 +22,17 @@ exports.help = (sender) => {
 
 exports.searchByCategory = (sender) => {
     messenger.getUserInfo(sender).then(response => {
-        let text = 'Sure thing, '+response.first_name+'. I can help you search our rates in two ways: You can either search by category or by uploading a picture; pick an option below.'
-        let postback = ['searchByCategory','searchByImage']
-        let options = ['By Categories','By Image'];
-        messenger.send(formatter.formatQuickReplies(text,postback,options), sender);
+        let text = 'Absolutely, '+response.first_name+'. Which category would you like to know more about?'
+        let projections = {"product_category":1};
+        producthandler.findProducts(projections).then(products => {
+            var postbacks = [];
+            var options = [];
+            for(var i = 0; i < products; i++) {
+                postbacks.push('displayCategory);
+                options.push(products.product_category);
+            }
+            messenger.send(formatter.formatQuickReplies(text,postbacks,options), sender);
+        });
     });
 };
 exports.searchByImage = (sender) => {
