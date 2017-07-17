3ed7313e8264bbfcad4574f7e8cda8c8d9fe3bb6
jkotak <kotakj@gmail.com>
2017-07-16 23:07:44 -0700

    Update product.js

--- models/product.js
+++ models/product.js
@@ -5,11 +5,11 @@ var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
   product_name: {type: String},
-  description: {type: String},
+  product_description: {type: String},
   product_category: {type: String},
-  top_range: {type: Number},
-  bottom_range: {type: Number},
-  photo_url: {type: String}
+  price_top_range: {type: Number},
+  price_bottom_range: {type: Number},
+  product_photo_url: {type: String}
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
